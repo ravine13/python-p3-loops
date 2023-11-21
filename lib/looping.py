d3b937804c60ed6010a2ec1e9fec050e762e398a
ravine13 <kuriaderek@gmail.com>
2023-11-21 22:32:50 +0300

    completed lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -=1
+        print("Happy New Year!")
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
-
+    squared_list = []
+    for num in int_list:
+        squared = num * num
+        squared_list.append(squared)
+    return squared_list
 def fizzbuzz():
-    # code goes here!
-    pass
+       for number in range(1, 101): 
+        if number % 3 == 0 and number % 5 == 0: 
+            print("FizzBuzz") 
+        elif number % 3 == 0:
+            print("Fizz") 
+        elif number % 5 == 0:
+            print("Buzz") 
+        else: 
+            print(number) 
